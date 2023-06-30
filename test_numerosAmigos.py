c13224bfa2ea213c8d55d818c1c98ed5b7f0c8c1
CarlosFrancoLL <carlos.franco.llanos@correounivalle.edu.co>
2023-06-30 18:52:48 -0500

    Se crean parametros para realizar una prueba con ayuda de unos números aleatorios

--- test_numerosAmigos.py
+++ test_numerosAmigos.py
@@ -1,8 +1,24 @@
+import pytest
+import random
 from numerosAmigos import numeros_amigos
+
+numero_random1 = random.randint(220, 1000000)
+numero_random2 = random.randint(220, 1000000)
 
 def test_numeros_amigos_1():
     assert numeros_amigos(1184,1210) == "1184 y 1210 son amigos"
+    print("La función numeros_amigos_1 funciona correctamente")
     
 def test_numeros_amigos_2():
     assert numeros_amigos(1184,1210) == "1184 y 1210 no son amigos"
+    print("La función numeros_amigos_2 funciona correctamente")
     
+@pytest.mark.parametrize(
+    "input_x, input_y, esperado",
+    [
+        (1500, 2000, "1500 y 2000 no son amigos")
+        (10744, 10856, "10744 y 10856 son amigos")
+        (numero_random1,numero_random2, "{numero_random1} y {número_random2} son amigos")
+        (numero_random1,numero_random2, "{numero_random1} y {número_random2} no son amigos")
+    ]
+)
