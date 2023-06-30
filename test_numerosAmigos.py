0e1af320e07e36c67d63dfd6a69ff39cb0ca3b74
CarlosFrancoLL <carlos.franco.llanos@correounivalle.edu.co>
2023-06-30 18:29:12 -0500

    Se añade test de numeros amigos 2

--- test_numerosAmigos.py
+++ test_numerosAmigos.py
@@ -1,4 +1,8 @@
 from numerosAmigos import numeros_amigos
 
-def test_numeros_amigos():
+def test_numeros_amigos_1():
     assert numeros_amigos(1184,1210) == "1184 y 1210 son amigos"
+    
+def test_numeros_amigos_2():
+    assert numeros_amigos(1184,1210) == "1184 y 1210 no son amigos"
+    
