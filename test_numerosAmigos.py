531f8434e8fbae2a5c5d27a97f9522ca3f37004e
CarlosFrancoLL <carlos.franco.llanos@correounivalle.edu.co>
2023-06-30 19:10:56 -0500

    Se añadieron dos pruebas usando los números 1000 y 2000

--- test_numerosAmigos.py
+++ test_numerosAmigos.py
@@ -16,3 +16,11 @@ def test_numeros_amigos_3():
 def test_numeros_amigos_4():
     assert numeros_amigos(2620, 2924) == "2620 y 2924 no son amigos"
     print("La función numeros_amigos_4 funciona correctamente")
+    
+def test_numeros_amigos_5():
+    assert numeros_amigos(1000, 2000) == "1000 y 2000 son amigos"
+    print("La función numeros_amigos_5 funciona correctamente")
+    
+def test_numeros_amigos_6():
+    assert numeros_amigos(1000, 2000) == "1000 y 2000 no son amigos"
+    print("La función numeros_amigos_6 funciona correctamente")
